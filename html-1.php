170754b0747bb360c708bd1ce6e8ea4a3c76f17f
mzaini30 <muhzaini30@gmail.com>
2022-02-10 02:24:16 +0800

    Upload

--- html-1.php
+++ html-1.php
@@ -4,9 +4,9 @@
 		<title>Tes Magic</title>
 		<meta charset="utf8"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
-		<script src="https://unpkg.com/vconsole@latest/dist/vconsole.min.js"></script>
+		<!--script src="https://unpkg.com/vconsole@latest/dist/vconsole.min.js"></script-->
 		<script>
-		  new VConsole
+		  // new VConsole
 		</script>
 	</head>
 	<body>
